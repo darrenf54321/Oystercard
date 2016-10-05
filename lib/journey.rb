6a627ad5348ed9d6d59a65c005bc8063dbaecda5
Darrenf <darrenf54321@gmail.com>
2016-10-05 15:49:18 +0100

    trying to work out journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,13 +1,38 @@
 class Journey
-    
-attr_reader :entry_station
 
-    def initialize(entry_station = "Manchester")
+attr_accessor :entry_station, :exit_station
+
+    def initialize
     @entry_station = nil
+    @exit_station = nil
     end
 
-    def start_journey(entry_station)
-    @entry_station = entry_station
+    def touch_in?(entry_station)
+        @entry_station = entry_station
     end
+
+    def touch_out?(exit_station)
+        @exit_station = exit_station
+        create_journey
+    end
+
+    def create_journey
+       last_journey = Hash.new
+       last_journey[:entry_station] = @entry_station
+       last_journey[:exit_station] = @exit_station
+       @travel_log << last_journey
+    end
+
+    def penalty
+        penalty = 10.00
+    end
+
 
 end
+
+
+# require './lib/journey'
+# journey = Journey.new
+# journey.entry_station("Manchester")
+# journey.exit_station("Birmingham")
+# journey
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -4,15 +4,29 @@
 =end
 
 require 'journey'
+require 'card'
 
 describe Journey do
-    
-    describe 'start_journey' do
-      it 'has an entry station' do
-      journey = Journey.new
-      expect(subject.start_journey).to eq "Manchester"
-      end
-end
-    
-end
 
+  describe 'initialize' do
+
+    it "will know when the card has been used to touch-in" do
+      card.topup(1.0)
+      expect(card.touch_in?(entry_station)).to eq true
+    end
+
+    it "will remember the station the card was touched-in" do
+      card.topup(50.00)
+      subject.touch_in?(entry_station)
+      expect(subject.entry_station).to eq entry_station
+      end
+    end
+
+  describe 'penalty' do
+      it 'will charge a penalty fare if a station is not tapped' do
+      journey = Journey.new
+      expect(journey.penalty).to eq 10.00
+    end
+  end
+
+end
