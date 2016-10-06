708d5c33cdc758e5ed32831834e99376267fd971
Darrenf <darrenf54321@gmail.com>
2016-10-06 11:12:07 +0100

    f

--- lib/feature_card.rb
+++ lib/feature_card.rb
@@ -3,6 +3,7 @@ require './lib/journey'
 card = Oystercard.new
 card.topup(5.00)
 card.touch_in("Exeter")
+card.touch_in("Balham")
 card.touch_out("London")
 card.touch_in("Balham")
 card.touch_out("Tooting")
